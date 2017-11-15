4b7a8b780cf79344fb20a52b452b750b4006cb72
robotsdancing <robotsdancing@gmail.com>
2017-11-14 23:48:45 -0700

    Fixed some stuff
    fixed the correct subtraction for arrays(was note-37, needed to be note-36), and corrected a minor issue with fastpwmdac(had the values set for the 10 bit dac, but it was instantiated as an 8 bit dac, which gave goofy results.)

--- CVMIDIConverter.ino
+++ CVMIDIConverter.ino
@@ -1,3 +1,4 @@
+
 #include <MIDI.h>
 #include <midi_Defs.h>
 #include <midi_Message.h>
@@ -10,8 +11,9 @@
 int subOscPin = 6;
 int cvPin = 9;
 int pwmDacVals[] = {51, 54, 57, 61, 64, 68, 72, 81, 86, 91, 97, 102, 108, 115, 122, 129, 137, 145, 153, 162, 172, 182, 193, 205, 217, 230, 243, 258, 273, 289, 307, 325, 344, 365, 386, 409};
-//Dac values 
-int noteFrequencies[] = {33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 93, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262};
+//Dac values calculat
+int noteFrequencies[] = {33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 93, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262};
+                        
 //note frequencies for the array are rounded and all the values are found from here http://bit.ly/2hm5oN8
 FastPWMdac fastPWMdac;
 // Created and binds the MIDI interface to the default hardware Serial port
@@ -36,9 +38,8 @@ void loop(){
     MIDI.read();
 }
 void handleNoteOn(byte channel, byte note, byte velocity){
-   fastPWMdac.analogWrite8bit(pwmDacVals[note-37]);
-        
-  tone(subOscPin,noteFrequencies[note-37]);//turn on note, acts as another oscillator.
+  fastPWMdac.analogWrite10bit(pwmDacVals[note-36]);     
+  tone(subOscPin,noteFrequencies[note-36]);//turn on note, acts as another oscillator.
   
 }
 void handleNoteOff(byte channel, byte note, byte velocity){
